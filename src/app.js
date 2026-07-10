91d5e4888c708e7d1c3630a58fc926816a59cca7
Pardhu Yadav <yadavpardhu44@gmail.com>
2026-07-10 08:06:53 +0530

    explored regex & dynamic urls

--- src/app.js
+++ src/app.js
@@ -2,24 +2,30 @@ const express = require("express");
 const app = express();
 
 
-app.get("/user", (req, res) => {
+app.get("/a{b}cd", (req, res) => {
     res.send({FirstName: "Pardhu", LastName: "Yadav"})
 })
 
-app.post("/user", (req, res) => {
-    res.send("data successfully saved to the DB.")
+app.get("/a*cd", (req, res) => {
+    res.send({FirstName: "Pardhu", LastName: "Yadav"})
 })
 
-app.delete("/user", (req, res) => {
-    res.send("Deleted successfully.")
+app.get(/a/, (req, res) => {
+    res.send({FirstName: "Pardhu", LastName: "Yadav"})
 })
 
-app.use("/hello",(req, res) => {
-    res.send("Hello hello 👋")
-});
+app.get(/.*fly$/, (req, res) => {
+    res.send({FirstName: "Pardhu", LastName: "Yadav"})
+})
 
-app.use("/user", (req, res) => {
-    res.send("Hahahaha")
+app.get("/user", (req, res) => {
+    console.log(req.query);
+    res.send({FirstName: "Pardhu", LastName: "Yadav"})
+})
+
+app.get("/user/:userid/:name/:password", (req, res) => {
+    console.log(req.params);
+    res.send({FirstName: "Pardhu", LastName: "Yadav"})
 })
 
 app.listen(7777, () => {
