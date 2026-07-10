dc4d8b08b048c20a3120e437e9ac4ba32520284a
Pardhu Yadav <yadavpardhu44@gmail.com>
2026-07-10 17:24:22 +0530

    wildcard-error-handling

--- src/app.js
+++ src/app.js
@@ -2,22 +2,30 @@ const express = require("express");
 const {adminAuthentication, userAuthentication} = require("./middlewares/auth")
 const app = express();
 
-app.use("/admin", adminAuthentication)
-app.get("/admin/data", (req, res) => {
-    res.send("This is the whole data")
-})
-app.delete("/admin/delete", (req, res) => {
-    res.send("Deleted the data")
-})
-
-app.get("/user/login", (req, res) => {
-    res.send("User loggedIn");
+app.use("/", (err, req, res, next) => {
+    if(err){
+        //log your error
+        res.status(500).send("something went wrong");
+    }
 })
 
-app.get("/user/data", userAuthentication, (req, res) => {
-    res.send("User data");
+app.get("/user", (req, res) => {
+    //try {
+        throw new Error("dksdk");
+        res.send("User data sent");
+    //}
+    // catch(err) {
+    //     if(err){
+    //         res.status(500).send("some error conatact support team")
+    //     }
+    // }
 })
-
+app.use("/", (err, req, res, next) => {
+    if(err){
+        //log your error
+        res.status(500).send("something went wrong");
+    }
+})
 
 app.listen(7777, () => {
     console.log("server started sucessully at 7777...")
