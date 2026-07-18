f8fd2be7a3a078e29db8b65c9cf78dda8dd463ba
Pardhu Yadav <yadavpardhu44@gmail.com>
2026-07-18 08:11:31 +0530

    GET /user/feed API

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,6 +3,7 @@ const userRouter = express.Router();
 const { userAuthentication } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 const USER_SAFE_DATA = "firstName lastName photourl gender age about skills";
+const User = require("../models/user");
 
 userRouter.get(
   "/user/requests/received",
@@ -36,12 +37,42 @@ userRouter.get("/user/connections", userAuthentication, async (req, res) => {
       .populate("fromUserId", USER_SAFE_DATA)
       .populate("toUserId", USER_SAFE_DATA);
     const data = connectionRequests.map((row) => {
-        if(row.toUserId._id.equals(loggedInuser._id)){
-            return row.fromUserId;
-        }
-        return row.toUserId;
+      if (row.toUserId._id.equals(loggedInuser._id)) {
+        return row.fromUserId;
+      }
+      return row.toUserId;
     });
     res.json({ data });
+  } catch (err) {
+    res.status(400).send("ERROR: " + err);
+  }
+});
+
+userRouter.get("/user/feed", userAuthentication, async (req, res) => {
+  try {
+    const loggedInuser = req.user;
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInuser._id }, { toUserId: loggedInuser._id }],
+    }).select("fromUserId toUserId");
+    const hideUserFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUserFromFeed.add(req.fromUserId.toString());
+      hideUserFromFeed.add(req.toUserId.toString());
+    });
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUserFromFeed) } },
+        { _id: { $ne: loggedInuser._id } },
+      ],
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+    res.send(users);
   } catch (err) {
     res.status(400).send("ERROR: " + err);
   }
